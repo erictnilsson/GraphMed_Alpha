ff0e15a7b2f50f1ba4711c6d0c3f66ed281f1c18
Eric Nilsson <ericnilssont@gmail.com>
2018-02-02 14:06:23 +0100

    Added FileHandler
    To Validate CSV

---
+++ GraphMed_Alpha/Handlers/FileHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphMed_Alpha.Handlers
+{
+    class FileHandler
+    {
+        public static void ValidateCSV(string filepath)
+        {
+            if (File.Exists(filepath))
+            {
+                string[] tmp1 = File.ReadAllLines(filepath);
+                string[] tmp = new string[0];
+
+                for (int j = 0; j < tmp1.Length; j++)
+                {
+                    string abc = "";
+                    tmp = tmp1[j].Split('\t');
+                    for (int i = 0; i < tmp.Length; i++)
+                    {
+                        var val = tmp[i];
+                        if (val.Contains("\""))
+                        {
+                            var a = val.Insert(val.IndexOf("\""), "\"");
+                            a = a.Insert(a.LastIndexOf("\""), "\"");
+                            var line = "\"" + a + "\"";
+                            tmp[i] = line;
+                        }
+                        abc += tmp[i] + "\t";
+                    }
+                    tmp1[j] = abc;
+                }
+
+                File.WriteAllLines(filepath, tmp1);
+            }
+        }
+    }
+}
--- GraphMed_Alpha/Program.cs
+++ GraphMed_Alpha/Program.cs
@@ -3,6 +3,7 @@ using GraphMed_Alpha.Handlers.CypherHandler;
 using GraphMed_Alpha.Model;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
@@ -19,14 +20,17 @@ namespace GraphMed_Alpha
             stopwatch.Start();
 
             // Testing
+
+            FileHandler.ValidateCSV("C:/Users/Eric Nilsson/Documents/Neo4j/default.graphdb/import/description_snapshot_deluxe.txt"); 
             CypherHandler.Load().Concepts();
             CypherHandler.Load().Descriptions(forceConceptRelation: false);
             CypherHandler.Create().Relationships(concept: new Concept(), description: new Description()); 
 
             // Diagnostics purposes
             stopwatch.Stop();
-            System.Diagnostics.Process.Start("http://127.0.0.1:7474/browser/");
+            //System.Diagnostics.Process.Start("http://127.0.0.1:7474/browser/");
             Console.WriteLine("Process completed in " + stopwatch.ElapsedMilliseconds + "ms");
+            Console.ReadLine(); 
         }
     }
 }
